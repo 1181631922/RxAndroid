633bd5131678865b5d8b8c5dc0b31ee5942bd9d1
fanyafeng <fanyafeng@365rili.com>
2016-06-16 10:40:31 +0800

    Initial commit

---
+++ app/src/main/java/com/fanyafeng/rxandroid/activity/MainActivity.java
@@ -0,0 +1,42 @@
+package com.fanyafeng.rxandroid.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.fanyafeng.rxandroid.BaseActivity;
+import com.fanyafeng.rxandroid.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        initView();
+        initData();
+    }
+
+    private void initView(){
+
+    }
+
+    private void initData(){
+        
+    }
+
+}
