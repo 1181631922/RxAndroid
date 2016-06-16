f88deab3adc4937c94f23d6754868dea8179c3d6
fanyafeng <fanyafeng@365rili.com>
2016-06-16 17:32:29 +0800

    重构代码

--- app/src/main/java/com/fanyafeng/rxandroid/BaseActivity.java
+++ app/src/main/java/com/fanyafeng/rxandroid/BaseActivity.java
@@ -8,50 +8,80 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class BaseActivity extends AppCompatActivity implements View.OnClickListener{
+
+    protected Toolbar toolbar;
+    protected FloatingActionButton fab;
+    protected TextView toolbar_center_title;
+    protected boolean isShowToolbar = true;
+    protected boolean isSetNavigationIcon = true;
+    protected boolean isSetLogo = false;
+    protected boolean isShowEmail = true;
+    protected String title;
+    protected String centertitle;
+    protected String subtitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+
 
     @Override
     public void onClick(View v) {
 
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_base, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar_center_title = (TextView) findViewById(R.id.toolbar_center_title);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        if (toolbar != null) {
+            if (isShowToolbar) {
+                setSupportActionBar(toolbar);
+            } else {
+                toolbar.setVisibility(View.GONE);
+            }
+
+            if (title != null && !title.equals("")) {
+                toolbar.setTitle(title);
+            }
+
+            if (subtitle != null && !subtitle.equals("")) {
+                toolbar.setSubtitle(subtitle);
+            }
+            if (isSetNavigationIcon) {
+//                由于要兼容低版本，所以采用这个划杠的方法
+                toolbar.setNavigationIcon(getResources().getDrawable(R.mipmap.ic_menu_back));
+                toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        finish();
+                    }
+                });
+            }
+            if (isSetLogo) {
+                toolbar.setLogo(getResources().getDrawable(R.mipmap.ic_menu_sort_by_size));
+            }
+
+            if (toolbar_center_title != null) {
+                if (centertitle != null && !centertitle.equals("")) {
+                    toolbar_center_title.setText(centertitle);
+                } else {
+                    toolbar_center_title.setText("");
+                }
+
+            }
+
+        }
+
+        if (fab != null) {
+            fab.setVisibility(isShowEmail ? View.VISIBLE : View.GONE);
+        }
+
+    }
 }
--- app/src/main/java/com/fanyafeng/rxandroid/activity/RetrofitActivity.java
+++ app/src/main/java/com/fanyafeng/rxandroid/activity/RetrofitActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
@@ -21,15 +22,13 @@ import retrofit.GsonConverterFactory;
 import retrofit.Response;
 import retrofit.Retrofit;
 
-public class MainActivity extends BaseActivity {
-
-
-
+public class RetrofitActivity extends BaseActivity {
+    private TextView tvShowInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_retrofit);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -46,7 +45,7 @@ public class MainActivity extends BaseActivity {
     }
 
     private void initView() {
-
+        tvShowInfo = (TextView) findViewById(R.id.tvShowInfo);
     }
 
     private void initData() {
@@ -57,11 +56,33 @@ public class MainActivity extends BaseActivity {
     public void onClick(View v) {
         super.onClick(v);
         switch (v.getId()) {
-            case R.id.btnRetrofit:
-                startActivity(new Intent(this, RetrofitActivity.class));
+            case R.id.btnSendRequest:
+                getData();
                 break;
         }
+
     }
 
+    private void getData() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(Urls.URL_TAOBAO_BASET)
+                .build();
+        ApiService apiService = retrofit.create(ApiService.class);
+        Call<TaoBaoGetIpInfoResponse> getIpInfoResponseCall = apiService.getIpInfoBean("63.223.108.42");
+        getIpInfoResponseCall.enqueue(new Callback<TaoBaoGetIpInfoResponse>() {
+            @Override
+            public void onResponse(Response<TaoBaoGetIpInfoResponse> response, Retrofit retrofit) {
+                Log.d("main", response.body().data.toString());
+                TaoBaoGetIpInfoResponse getIpInfoResponse = response.body();
+                tvShowInfo.setText(getIpInfoResponse.data.country);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                tvShowInfo.setText(t.getMessage());
+            }
+        });
+    }
 
 }
